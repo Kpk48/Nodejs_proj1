8330ba4ad40ab10f8ec478aea02dc0439a17830e
Kpk48 <praveenkuamrk2006@gmail.com>
2025-09-27 09:19:19 +0530

    First_projv2

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const http=require('http');
 const url=require('url');
 const fs=require('fs');
 const overview=fs.readFileSync('./starter/templates/template_overview.html','utf-8');
-const product=fs.readFileSync('./starter/templates/template_product.html','utf-8');
+const tempProduct=fs.readFileSync('./starter/templates/template_product.html','utf-8');
 const cards=fs.readFileSync('./starter/templates/template_cards.html','utf-8');
 const data=fs.readFileSync('./starter/dev-data/data.json','utf-8');
-const dataobj=JSON.parse(data);
+const dataObj=JSON.parse(data);
 const replaceElement=(temp,obj)=>{
 let output=temp.replace(/{%PRODUCTNAME%}/g,obj.productName);
 output=output.replace(/{%IMAGE%}/g,obj.image);
@@ -15,23 +15,50 @@ output=output.replace(/{%NUTRIENTS%}/g,obj.nutrients);
 output=output.replace(/{%QUANTITY%}/g,obj.quantity);
 output=output.replace(/{%DESCRIPTION%}/g,obj.description);
 output=output.replace(/{%ID%}/g,obj.id);
+
 if(!obj.organic){
     output=output.replace(/{%NOT_ORGANIC%}/g,'not-organic');
 }
 return output;
 }
-const server=http.createServer((req,res,err)=>{
-   const url3=req.url;
-   if(url3==='/' || url3==='/overview'){
-       res.writeHead(200,{'Content-Type':'text/html'});
-       const cards_rep =dataobj.map(el=> replaceElement(cards,el)).join('');
-       const output=overview.replace(/{%PRODUCT_CARDS%}/g,cards_rep)
-       console.log(cards_rep);
-       res.end(output);
-   }
-   else if(url3==='/products'){}
-});
+const server = http.createServer((req, res) => {
+    const { query, pathname } = url.parse(req.url, true);
 
+    // Overview page
+    if (pathname === '/' || pathname === '/overview') {
+        res.writeHead(200, {
+            'Content-type': 'text/html'
+        });
+
+        const cardsHtml = dataObj.map(el => replaceElement(cards, el)).join('');
+        const output = overview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+        res.end(output);
+
+        // Product page
+    } else if (pathname === '/product') {
+        res.writeHead(200, {
+            'Content-type': 'text/html'
+        });
+        const product = dataObj[query.id];
+        const output = replaceElement(tempProduct, product);
+        res.end(output);
+
+        // API
+    } else if (pathname === '/api') {
+        res.writeHead(200, {
+            'Content-type': 'application/json'
+        });
+        res.end(data);
+
+        // Not found
+    } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'hello-world'
+        });
+        res.end('<h1>Page not found!</h1>');
+    }
+});
 server.listen(8000,()=>{
   console.log(`Server started on port ${server.address().port}`);
 });
